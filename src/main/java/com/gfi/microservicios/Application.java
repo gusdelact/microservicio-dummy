6af5c022db13661417d26cee69acf555f45fbcd9
Gustavo De la Cruz Tovar <gusdelact@Gustavo-De-la-Cruz-Tovars-MacBook.local>
2018-05-27 17:51:59 -0500

    Servicio Dummy con proteccion con OAuth2

--- src/main/java/com/gfi/microservicios/Application.java
+++ src/main/java/com/gfi/microservicios/Application.java
@@ -2,16 +2,14 @@ package com.gfi.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 //@EnableEurekaClient
 @EnableDiscoveryClient
 //@EnableCircuitBreaker
+@EnableResourceServer
 public class Application {
 	  
 	public static void main(String[] args) {
